280d63678312310e449d2c521fed515937af24d4
MarkKoz <KozlovMark@gmail.com>
2016-09-04 23:15:01 -0700

    Initial commit

--- File-Date-Replacer/Program.cs
+++ File-Date-Replacer/Program.cs
@@ -1,28 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.IO;
-using System.Linq;
 
 namespace File_Date_Replacer
 {
 	public class Program
 	{
+		[SuppressMessage("ReSharper", "PossibleNullReferenceException")]
+		[SuppressMessage("ReSharper", "AssignNullToNotNullAttribute")]
 		public static void WriteDate(string p)
 		{
+			string nameExt = Path.GetFileName(p);
+			string name = Path.GetFileNameWithoutExtension(p);
+			string dateStr = name.Substring(0, 8);
+			const string dateFormat = "yyyyMMdd";
+			DateTime dateParsed;
+
 			if (!File.Exists(p))
 			{
-				Console.WriteLine("The specified file " +
-				                  $"'{Path.GetFileName(p)}' was not found.");
+				Console.WriteLine("[ERROR] The specified file " +
+				                  $"'{nameExt}' was not found.");
+				return;
 			}
-			else
+
+			if (!DateTime.TryParseExact(dateStr, dateFormat, null,
+				DateTimeStyles.None, out dateParsed))
 			{
-				DateTime dateAcc = File.GetLastAccessTime(p);
-				File.SetLastWriteTime(p, dateAcc);
-				Console.WriteLine("Set field 'Modified' of file " +
-									  $"'{Path.GetFileName(p)}' from " +
-									  $"{File.GetLastWriteTime(p)}" +
-									  $" to {dateAcc}");
+				Console.WriteLine("[ERROR] Unable to parse date " +
+				                  $"'{dateStr}' for file '{nameExt}'.");
+				return;
 			}
+
+			File.SetLastWriteTime(p, dateParsed);
+			File.SetLastAccessTime(p, dateParsed);
+			/*Console.WriteLine("[INFO] Set field 'Modified' of file " +
+									$"'{Path.GetFileName(p)}' from " +
+									$"{File.GetLastWriteTime(p)}" +
+									$" to {dateParsed}");*/
+
+			string nameNew = nameExt.Substring(9);
+			string pathNew = Path.Combine(Path.GetDirectoryName(p), nameNew);
+			File.Move(p, pathNew);
+			Console.WriteLine("[INFO] Successfully changed dates of " +
+			                  $"file '{nameExt}' to '{dateParsed}'.");
 		}
 
 		public static void Main(string[] args)
@@ -31,33 +52,52 @@ namespace File_Date_Replacer
 
 			if (args.Length <= 0)
 			{
-				Console.WriteLine("No path was specified.");
+				Console.WriteLine("[ERROR] No path was specified.");
 				Console.ReadLine();
 			}
 			else
 			{
+				/*try
+				{
+					if (File.GetAttributes(args[0]).HasFlag(FileAttributes.Directory))
+					{
+
+					}
+				}
+				catch (FileNotFoundException)
+				{
+					Console.WriteLine("[ERROR] The specified file " +
+					                  "was not found.");
+				}
+				catch (DirectoryNotFoundException)
+				{
+					Console.WriteLine("[ERROR] The specified directory " +
+					                  "was not found.");
+				}*/
+
 				if (args[0].Contains("*"))
 				{
-					var files = Directory.EnumerateFiles(dir, args[0]);
-					Console.WriteLine($"{files.Count()} " +
-					              $"files were found in {dir}\n");
+					var files = Directory.GetFiles(dir, args[0]);
+					Console.WriteLine($"[INFO] {files.Length} " +
+									  $"files matching criteria '{args[0]}' " +
+					                  $"were found in '{dir}'.\n");
 
 					foreach (string f in files)
 					{
 						WriteDate(f);
 					}
 
-					Console.WriteLine("\nReplacing complete.");
+					Console.WriteLine("\n[INFO] Replacing complete.");
 					Console.ReadLine();
 				}
 				else
 				{
 					string path = Path.Combine(dir, args[0]);
-					Console.WriteLine($"The path is: {path}");
+					Console.WriteLine($"[INFO] The path is: {path}");
 
 					WriteDate(path);
 
-					Console.WriteLine("\nReplacing complete.");
+					Console.WriteLine("\n[INFO] Replacing complete.");
 					Console.ReadLine();
 				}
 			}
